b49b126687cd9d314a1fba2f08b4cba32422d84b
Muhammadazim <shuhrataliyevm@gmail.com>
2025-04-17 12:52:36 +0500

    nega bu xatoliklar bora

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@styles': path.resolve(__dirname, './src/styles')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@styles/_variables.scss" as *;`
-      }
-    }
-  }
 })
